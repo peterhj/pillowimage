e155a8b28bb6ffbbb74f95935f08db505e9b7963
Peter Jin <peterhaijin@gmail.com>
2018-02-25 16:56:54 -0800

    Initial commit.

---
+++ build.rs
@@ -0,0 +1,150 @@
+extern crate bindgen;
+extern crate cc;
+extern crate walkdir;
+
+use walkdir::{WalkDir};
+
+use std::env;
+use std::mem::{size_of};
+use std::os::raw::{c_short, c_int, c_long, c_longlong};
+use std::path::{PathBuf};
+
+fn main() {
+  let manifest_dir = PathBuf::from(env::var("CARGO_MANIFEST_DIR").unwrap());
+  let out_dir = PathBuf::from(env::var("OUT_DIR").unwrap());
+
+  println!("cargo:rustc-link-search=native={}", out_dir.display());
+  println!("cargo:rustc-link-lib=static=pillowimage_native");
+  println!("cargo:rerun-if-changed=build.rs");
+
+  //let imaging_src_dir = manifest_dir.join("pillow").join("src").join("libImaging");
+  let imaging_src_dir = manifest_dir.join("pillow-simd").join("libImaging");
+  let mut imaging_src_paths = vec![];
+  for entry in WalkDir::new(imaging_src_dir.to_str().unwrap()) {
+    let entry = entry.unwrap();
+    match entry.path().extension() {
+      None => continue,
+      Some(ext) => if ext.to_str().unwrap() == "c" {
+        if entry.path().file_name().unwrap().to_str().unwrap() == "codec_fd.c" {
+          continue;
+        }
+        if entry.path().file_name().unwrap().to_str().unwrap().contains("Decode") {
+          continue;
+        }
+        if entry.path().file_name().unwrap().to_str().unwrap().contains("Encode") {
+          continue;
+        }
+        if entry.path().file_name().unwrap().to_str().unwrap().contains("FilterSIMD") {
+          continue;
+        }
+        if entry.path().file_name().unwrap().to_str().unwrap().contains("ResampleSIMD") {
+          continue;
+        }
+        println!("cargo:rerun-if-changed={}", entry.path().display());
+        imaging_src_paths.push(entry.path().as_os_str().to_str().unwrap().to_owned());
+      } else if ext.to_str().unwrap() == "h" {
+        println!("cargo:rerun-if-changed={}", entry.path().display());
+      } else {
+        continue;
+      }
+    }
+  }
+
+  let sz_c_short = size_of::<c_short>();
+  let sz_c_int = size_of::<c_int>();
+  let sz_c_long = size_of::<c_long>();
+  let sz_c_longlong = size_of::<c_longlong>();
+
+  let mut compiler = cc::Build::new();
+  compiler
+    .opt_level(2)
+    .pic(true)
+    .flag("-g")
+    //.flag("-fwrapv")
+    //.flag("-fstack-protector-strong")
+    .flag("-fno-strict-aliasing")
+    .flag("-msse4.1")
+    .flag("-pthread")
+    //.flag("-D_FORTIFY_SOURCE=2")
+    .flag("-DPILLOW_VERSION='5.1.0.dev0'")
+    .flag("-DPILLOW_DISABLE_PYTHON=1")
+
+    // FIXME: these macros are normally provided by cpython?
+    .flag("-DHAVE_PROTOTYPES")
+    .flag("-DSTDC_HEADERS")
+    .flag("-DNDEBUG")
+    .flag(&format!("-DSIZEOF_SHORT={}", sz_c_short))
+    .flag(&format!("-DSIZEOF_INT={}", sz_c_int))
+    .flag(&format!("-DSIZEOF_LONG={}", sz_c_long))
+    .flag(&format!("-DSIZEOF_LONG_LONG={}", sz_c_longlong))
+
+    .flag("-Wall")
+    .flag("-Werror")
+    .flag("-Wno-sign-compare")
+    .flag("-Wno-unused-parameter")
+    //.flag("-Wformat")
+    //.flag("-Wstrict-prototypes")
+    //.flag("-Werror=format-security")
+    .include(imaging_src_dir.as_os_str().to_str().unwrap())
+    //.include("/usr/include")
+  ;
+  for path in imaging_src_paths.iter() {
+    compiler.file(path);
+  }
+  compiler.compile("libpillowimage_native.a");
+
+  bindgen::Builder::default()
+    .header("wrapped.h")
+    .clang_arg("-DPILLOW_DISABLE_PYTHON")
+    .clang_arg("-DHAVE_PROTOTYPES")
+    .clang_arg("-DSTDC_HEADERS")
+    .clang_arg("-DNDEBUG")
+    .clang_arg(format!("-DSIZEOF_SHORT={}", sz_c_short))
+    .clang_arg(format!("-DSIZEOF_INT={}", sz_c_int))
+    .clang_arg(format!("-DSIZEOF_LONG={}", sz_c_long))
+    .clang_arg(format!("-DSIZEOF_LONG_LONG={}", sz_c_longlong))
+    .clang_arg(format!("-I{}", imaging_src_dir.as_os_str().to_str().unwrap()))
+    //.clang_arg("-I/usr/include")
+    .whitelist_type("Imaging")
+    //.whitelist_type("ImagingAccess")
+    //.whitelist_type("ImagingCodecState")
+    .whitelist_type("ImagingSectionCookie")
+    .whitelist_type("ImagingShuffle")
+    //.whitelist_type("GIFDECODERSTATE_T")
+    //.whitelist_type("JPEGSTATE_T")
+    //.whitelist_type("ZIPSTATE_T")
+    .whitelist_function("ImagingSectionEnter")
+    .whitelist_function("ImagingSectionLeave")
+    .whitelist_function("ImagingNew")
+    .whitelist_function("ImagingNew2")
+    .whitelist_function("ImagingDelete")
+    //.whitelist_function("ImagingAccessNew")
+    //.whitelist_function("_ImagingAccessDelete")
+    .whitelist_function("ImagingFlipLeftRight")
+    .whitelist_function("ImagingFlipTopBottom")
+    .whitelist_function("ImagingRotate90")
+    .whitelist_function("ImagingRotate180")
+    .whitelist_function("ImagingRotate270")
+    .whitelist_function("ImagingResample")
+    .whitelist_function("ImagingTransform")
+    .whitelist_var("IMAGING_TYPE_UINT8")
+    .whitelist_var("IMAGING_TYPE_INT32")
+    .whitelist_var("IMAGING_TYPE_FLOAT32")
+    .whitelist_var("IMAGING_TYPE_SPECIAL")
+    //.whitelist_var("IMAGING_CODEC_END")
+    //.whitelist_var("IMAGING_CODEC_OVERRUN")
+    //.whitelist_var("IMAGING_CODEC_BROKEN")
+    //.whitelist_var("IMAGING_CODEC_UNKNOWN")
+    //.whitelist_var("IMAGING_CODEC_CONFIG")
+    //.whitelist_var("IMAGING_CODEC_MEMORY")
+    .whitelist_var("IMAGING_TRANSFORM_NEAREST")
+    .whitelist_var("IMAGING_TRANSFORM_BOX")
+    .whitelist_var("IMAGING_TRANSFORM_BILINEAR")
+    .whitelist_var("IMAGING_TRANSFORM_HAMMING")
+    .whitelist_var("IMAGING_TRANSFORM_BICUBIC")
+    .whitelist_var("IMAGING_TRANSFORM_LANCZOS")
+    .generate()
+    .unwrap()
+    .write_to_file(out_dir.join("imaging_bind.rs"))
+    .unwrap();
+}
---
+++ src/lib.rs
@@ -0,0 +1,166 @@
+#[macro_use]
+extern crate lazy_static;
+
+use ffi::*;
+
+use std::ffi::{CString};
+use std::os::raw::{c_char};
+use std::ptr::{null_mut};
+
+pub mod ffi;
+
+lazy_static! {
+  static ref MODE_1:        CString = CString::new("1").unwrap();
+  static ref MODE_L:        CString = CString::new("L").unwrap();
+  static ref MODE_P:        CString = CString::new("P").unwrap();
+  static ref MODE_I:        CString = CString::new("I").unwrap();
+  static ref MODE_F:        CString = CString::new("F").unwrap();
+  static ref MODE_RGB:      CString = CString::new("RGB").unwrap();
+  static ref MODE_RGBA:     CString = CString::new("RGBA").unwrap();
+  static ref MODE_CMYK:     CString = CString::new("CMYK").unwrap();
+  static ref MODE_YCBCR:    CString = CString::new("YCbCr").unwrap();
+  static ref MODE_LAB:      CString = CString::new("LAB").unwrap();
+}
+
+#[derive(Clone, Copy, PartialEq, Eq, Debug)]
+pub enum PILType {
+  Uint8,
+  Int32,
+  Float32,
+}
+
+impl PILType {
+  pub fn from_raw(raw: u32) -> Self {
+    match raw {
+      IMAGING_TYPE_UINT8    => PILType::Uint8,
+      IMAGING_TYPE_INT32    => PILType::Int32,
+      IMAGING_TYPE_FLOAT32  => PILType::Float32,
+      IMAGING_TYPE_SPECIAL  => unimplemented!(),
+      _ => unreachable!(),
+    }
+  }
+
+  pub fn to_raw(&self) -> u32 {
+    match *self {
+      PILType::Uint8    => IMAGING_TYPE_UINT8,
+      PILType::Int32    => IMAGING_TYPE_INT32,
+      PILType::Float32  => IMAGING_TYPE_FLOAT32,
+    }
+  }
+}
+
+#[derive(Clone, Copy, PartialEq, Eq, Debug)]
+pub enum PILMode {
+  Unit,
+  L,
+  P,
+  I,
+  F,
+  RGB,
+  RGBA,
+  CMYK,
+  YCbCr,
+  LAB,
+}
+
+impl PILMode {
+  pub fn to_raw(&self) -> *const c_char {
+    match *self {
+      PILMode::Unit     => MODE_1.as_c_str().as_ptr(),
+      PILMode::L        => MODE_L.as_c_str().as_ptr(),
+      PILMode::P        => MODE_P.as_c_str().as_ptr(),
+      PILMode::I        => MODE_I.as_c_str().as_ptr(),
+      PILMode::F        => MODE_F.as_c_str().as_ptr(),
+      PILMode::RGB      => MODE_RGB.as_c_str().as_ptr(),
+      PILMode::RGBA     => MODE_RGBA.as_c_str().as_ptr(),
+      PILMode::CMYK     => MODE_CMYK.as_c_str().as_ptr(),
+      PILMode::YCbCr    => MODE_YCBCR.as_c_str().as_ptr(),
+      PILMode::LAB      => MODE_LAB.as_c_str().as_ptr(),
+    }
+  }
+}
+
+#[derive(Clone, Copy, PartialEq, Eq, Debug)]
+pub enum PILFilter {
+  Nearest,
+  Box_,
+  Bilinear,
+  Hamming,
+  Bicubic,
+  Lanczos,
+}
+
+impl PILFilter {
+  pub fn to_raw(&self) -> u32 {
+    match *self {
+      PILFilter::Nearest    => IMAGING_TRANSFORM_NEAREST,
+      PILFilter::Box_       => IMAGING_TRANSFORM_BOX,
+      PILFilter::Bilinear   => IMAGING_TRANSFORM_BILINEAR,
+      PILFilter::Hamming    => IMAGING_TRANSFORM_HAMMING,
+      PILFilter::Bicubic    => IMAGING_TRANSFORM_BICUBIC,
+      PILFilter::Lanczos    => IMAGING_TRANSFORM_LANCZOS,
+    }
+  }
+}
+
+pub struct PILImage {
+  ptr:  Imaging,
+}
+
+unsafe impl Send for PILImage {}
+//unsafe impl Sync for PILImage {}
+
+impl Drop for PILImage {
+  fn drop(&mut self) {
+    assert!(!self.ptr.is_null());
+    unsafe { ImagingDelete(self.ptr) };
+  }
+}
+
+impl PILImage {
+  pub fn new(mode: PILMode, xdim: i32, ydim: i32) -> Self {
+    PILImage{
+      ptr:  unsafe { ImagingNew(mode.to_raw(), xdim, ydim) },
+    }
+  }
+
+  pub unsafe fn from_raw(raw_im: Imaging) -> Self {
+    PILImage{
+      ptr:  raw_im,
+    }
+  }
+
+  pub fn width(&self) -> i32 {
+    unsafe { (*self.ptr).xsize }
+  }
+
+  pub fn height(&self) -> i32 {
+    unsafe { (*self.ptr).ysize }
+  }
+
+  pub fn pixel_type(&self) -> PILType {
+    PILType::from_raw(unsafe { (*self.ptr).type_ } as u32)
+  }
+
+  pub fn pixel_channels(&self) -> i32 {
+    unsafe { (*self.ptr).bands }
+  }
+
+  pub fn pixel_size_bytes(&self) -> i32 {
+    unsafe { (*self.ptr).pixelsize }
+  }
+
+  pub fn line_size_bytes(&self) -> i32 {
+    unsafe { (*self.ptr).linesize }
+  }
+
+  pub fn resample(&self, new_xdim: i32, new_ydim: i32, filter: PILFilter) -> Self {
+    self.resample_crop(new_xdim, new_ydim, filter, [0.0, 0.0, new_xdim as _, new_ydim as _])
+  }
+
+  pub fn resample_crop(&self, new_xdim: i32, new_ydim: i32, filter: PILFilter, mut new_crop: [f32; 4]) -> Self {
+    PILImage{
+      ptr:  unsafe { ImagingResample(self.ptr, new_xdim, new_ydim, filter.to_raw() as i32, (&mut new_crop).as_mut_ptr()) },
+    }
+  }
+}
